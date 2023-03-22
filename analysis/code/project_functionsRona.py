1c7dacb1e75aed1971b209cfb7c93c64ee09ecf5
RONA HABIBI <RONARONIAK@GMAIL.com>
2023-03-22 15:23:01 -0700

    Rona's project functions added

---
+++ analysis/code/project_functionsRona.py
@@ -0,0 +1,56 @@
+#load import statements
+import pandas as pd
+import matplotlib.plyplot as plt
+import seaborn as sns
+
+
+
+#starting the following will be for the Natural Disasters Data
+# Here is the cleaned dataset for Natural Disasters from 1970-2021
+def processed_nd_data():
+    dfclean = (pd.read_csv("../data/raw/1900_2021_DISASTERS.csv")
+           .query('`Disaster Type` in ["Flood", "Storm", "Drought", "Wildfire", "Landslide"]')
+           .iloc[:, [2, 6, 7, 11, 13, 14]]
+           .dropna()
+          )
+    return dfclean 
+
+# These next two are rather similar; they will display the number of natural disasters per year including both the number and percentage of increase or decrease in events. This first one groups the data by Continent where the second one does not 
+
+def num_nd_continent():
+    df_ndperyear = (dfclean.groupby(['Year', 'Continent', 'Disaster Type'])
+                .size()
+                .reset_index(name='Number of Disasters')
+                .assign(Difference_from_Previous_Year=lambda x: x.groupby('Disaster Type')['Number of Disasters'].diff(),
+                        Percent_Difference_from_Previous_Year=lambda x: (x['Difference_from_Previous_Year'] / x.groupby('Disaster Type')['Number of Disasters'].shift(1)) * 100)
+               )
+    return df_ndperyear
+
+
+def num_nd_yearly():
+    df_ndperyear = (dfclean.groupby(['Year', 'Disaster Type'])
+                .size()
+                .reset_index(name='Number of Disasters')
+                .assign(Difference_from_Previous_Year=lambda x: x.groupby('Disaster Type')['Number of Disasters'].diff(),
+                        Percent_Difference_from_Previous_Year=lambda x: (x['Difference_from_Previous_Year'] / x.groupby('Disaster Type')['Number of Disasters'].shift(1)) * 100)
+                   )
+    return df_ndperyear
+
+#The following will be for the Emissions Data:
+
+def processed_emi_data():
+     data_em = (pd.read_csv("../data/raw/owid_emissions.csv")
+                .query('year >= 1970')
+                .iloc[:, [0, 1, 3, 7, 9]]
+                .dropna()
+                )
+    return df_em      
+             
+             
+             
+             
+             
+             
+             
+             
+             
