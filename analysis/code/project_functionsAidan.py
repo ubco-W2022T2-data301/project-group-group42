8e035a896f38493dad9719d410ad3febd20be529
Aidanell <aidan.elliott29@gmail.com>
2023-03-25 13:16:04 -0700

    properly imported my functions

--- analysis/code/project_functionsAidan.py
+++ analysis/code/project_functionsAidan.py
@@ -9,30 +9,25 @@ def process_emission_data():
             .loc[lambda x: x.population >= 30000000] # only countries with >30mil people
             .iloc[:,[0,1,3,4,7,9]]) #only columns that we need
 
+
 def process_temp_dataset():
     temp_df = pd.read_csv("../data/raw/GlobalLandTemperaturesByCountry.csv")
     temp_df["dt"] = (temp_df["dt"].apply(pd.to_datetime)).dt.date #Converts to datetime (Takes a while)
-    temp_df["year"] = [x.year for x in temp_df["dt"]]
-    temp_df = temp_df.groupby([temp_df["year"],temp_df["Country"]])["AverageTemperature"].mean(numeric_only=True).reset_index()
-    return temp_df
-
-def pivoted_temp_dataset():
-    temp_df = pd.read_csv("../data/raw/GlobalLandTemperaturesByCountry.csv")
-    temp_df["dt"] = (temp_df["dt"].apply(pd.to_datetime)).dt.date #Converts to datetime (Takes a while)
     temp_df["year"] = [x.year for x in temp_df["dt"]] #Just shows the year
-    temp_df = (temp_df.groupby([temp_df["year"],temp_df["Country"]])["AverageTemperature"].mean(numeric_only=True).reset_index() #Convertes monthly average temp into annaully
-            .sort_values(by=["Country","year"])
+    temp_df = (temp_df.groupby([temp_df["year"],temp_df["Country"]])["AverageTemperature"].mean(numeric_only=True).reset_index()#Convertes monthly average temp into annaully
+               
+    pivoted_df = (tempdf.sort_values(by=["Country","year"])
             .loc[lambda x: x.year > 1959] #Drops rows from before 1960
             .replace({"United States":"United States of America", #Changes a few country names for plotting with geopandas
                      "Congo (Democratic Republic Of The)" : "Dem. Rep. Congo",
                      "Central African Republic":"Central African Rep.",
                      "Côte D'Ivoire":"Côte d'Ivoire",
                      "Czech Republic":"Czechia"}))
+              )
     
     world = geopandas.read_file(geopandas.datasets.get_path('naturalearth_lowres')).sort_values(by=["name"]) #loads world dataset from geopandas
-    temp_df = temp_df[temp_df["Country"].isin(world["name"].unique().tolist())] #Gets rid of any country which is not in world data (basically small island nations)
-    
-    pivoted_df = temp_df.pivot(index="Country",columns=["year"],values=["AverageTemperature"]) #pivots so each country is a row and the temperature at each year is the columns
+    pivoted_df = pivoted_df[pivoted_df["Country"].isin(world["name"].unique().tolist())] #Gets rid of any country which is not in world data (basically small island nations)
+    pivoted_df = pivoted_df.pivot(index="Country",columns=["year"],values=["AverageTemperature"]) #pivots so each country is a row and the temperature at each year is the columns
     pivoted_df["delta_temp"] = pivoted_df.iloc[:,53] - pivoted_df.iloc[:,0] #Calculates change in temperature from 1960 to 2013
     
-    return pivoted_df
+    return temp_df, pivoted_df
